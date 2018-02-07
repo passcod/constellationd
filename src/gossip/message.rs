596b870cb4854411f074078179d37e333888fea7
Félix Saparelli <felix@passcod.name>
2018-02-07 23:34:07 +1300

    Add conveniences to Message

--- src/gossip/message.rs
+++ src/gossip/message.rs
@@ -8,12 +8,21 @@ pub enum Kind {
     Pong,
 }
 
+impl Kind {
+    pub fn is_ping(&self) -> bool {
+        self == &Kind::Ping
+    }
+}
+
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct Message {
     pub agent: (String, String),
     pub id: String,
 
     pub kind: Kind,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub seq: Option<usize>,
 }
 
 impl Message {
@@ -25,6 +34,17 @@ impl Message {
                 constants::AGENT.1.into()
             ),
             kind: kind,
+            seq: None,
         }
+    }
+
+    pub fn hello() -> Self {
+        Self::new(Kind::Hello)
+    }
+
+    pub fn pong(seq: usize) -> Self {
+        let mut msg = Self::new(Kind::Pong);
+        msg.seq = Some(seq);
+        msg
     }
 }
