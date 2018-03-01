fbe25241e37462ab8be7f531e076062b290f2528
Félix Saparelli <felix@passcod.name>
2018-03-02 01:18:08 +1300

    Finish generic Decoder

--- src/operator/codec.rs
+++ src/operator/codec.rs
@@ -1,28 +1,80 @@
 use bytes::BytesMut;
+use constants;
 use errors::SendError;
+use rust_sodium::crypto::secretbox::{gen_nonce, open, Nonce, seal};
+use serde_cbor;
+use statics;
+use std::slice::Iter;
 use std::io;
 use tokio_io::codec::{Decoder, Encoder};
 
 use super::envelope::Envelope;
 
-#[inline]
-fn io_error(message: String) -> io::Error {
-    io::Error::new(io::ErrorKind::Other, message)
-}
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct OperatorCodec {
     header_len: Option<u8>,
+    nonce: Option<Nonce>,
     payload_len: Option<usize>,
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
+struct Header (String, Nonce, usize);
 
 impl Default for OperatorCodec {
     fn default() -> Self {
         Self {
             header_len: None,
+            nonce: None,
             payload_len: None,
         }
     }
+}
+
+fn io_error(message: String) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, message)
+}
+
+fn take_version(buf: &BytesMut) -> Result<Option<()>, io::Error> {
+    match buf.get(0) {
+        None => Ok(None),
+        Some(&constants::PROTOCOL_VERSION) => Ok(Some(())),
+        Some(v) => {
+            buf.clone().advance(1);
+            Err(io_error(format!("bad version: {}", v)))
+        },
+    }
+}
+
+fn take_header_len(buf: &BytesMut) -> Result<Option<u8>, io::Error> {
+    match buf.get(1) {
+        None => Ok(None),
+        Some(&0) => {
+            buf.clone().advance(2);
+            Err(io_error("zero length header".into()))
+        },
+        Some(h) => Ok(Some(*h)),
+    }
+}
+
+fn take_header(buf: &BytesMut, length: u8) -> Result<Option<Header>, io::Error> {
+    let length = length as usize;
+    let hbuf = &buf[2..(length + 2)];
+    if hbuf.len() != length {
+        return Ok(None);
+    }
+
+    return match serde_cbor::from_slice(hbuf) {
+        Err(err) => {
+            let serr = format!("{:?}", err);
+            if serr.starts_with("ErrorImpl { code: EofWhileParsing") {
+                Ok(None) // incomplete input
+            } else {
+                buf.clone().advance(2 + length);
+                Err(io_error(serr))
+            }
+        },
+        Ok(e) => Ok(Some(e))
+    };
 }
 
 impl Decoder for OperatorCodec {
@@ -34,28 +86,61 @@ impl Decoder for OperatorCodec {
             return Ok(None) // request more data
         }
 
-        let v = buf.get(0);
-        if v.is_none() { return Ok(None) }
-        let v = v.unwrap() as &u8;
-        if v != &0 { return Err(io_error("bad version".into())) }
+        if self.payload_len.is_none() {
+            if self.header_len.is_none() {
+                if take_version(buf)?.is_none() {
+                    return Ok(None);
+                }
 
-        let hlen = buf.get(1);
-        if hlen.is_none() { return Ok(None) }
-        let hlen = hlen.unwrap() as &u8;
-        if hlen == &0 { return Err(io_error("bad header length".into())) }
+                if let Some(h) = take_header_len(buf)? {
+                    self.header_len = Some(h);
+                } else {
+                    return Ok(None);
+                }
+            }
 
-        self.header_len = Some(*hlen);
+            if let Some(header) = take_header(buf, self.header_len.unwrap())? {
+                let total = 2 + (self.header_len.unwrap() as usize) + header.2;
 
-        let env = match Envelope::unpack(buf)? {
-            None => return Ok(None), // incomplete input
-            Some(e) => e
-        };
+                if &header.0 != statics::key() {
+                    buf.clone().advance(total);
+                    return Err(io_error("invalid key".into()));
+                }
 
-        if ! env.check() {
-            return Err(io_error(format!("Bad metadata: {:?}", env)))
+                if header.2 == 0 {
+                    buf.clone().advance(total);
+                    return Err(io_error("zero length payload".into()));
+                }
+
+                self.nonce = Some(header.1);
+                self.payload_len = Some(header.2);
+            } else {
+                return Ok(None);
+            }
         }
 
-        env.open().map(|b| Some(b))
+        let start = 2 + (self.header_len.unwrap() as usize);
+        let length = self.payload_len.unwrap();
+        let total_expected = start + length;
+        if buf.len() < total_expected {
+            return Ok(None);
+        }
+
+        let pbuf = &buf[start..(start + length)];
+        if pbuf.len() < length {
+            return Ok(None); // just checking...
+        }
+
+        match open(pbuf, &self.nonce.unwrap(), statics::secret()) {
+            Err(_) => {
+                buf.clone().advance(total_expected);
+                Err(io_error("bad payload encryption".into()))
+            },
+            Ok(payload) => {
+                buf.clone().advance(total_expected);
+                Ok(Some(payload))
+            }
+        }
     }
 }
 
